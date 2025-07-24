aacf38bde17994fc67051b4fcfb039e5d16fbfc3
kkang4557 <kkang45597@gmail.com>
2025-07-24 15:50:55 +0900

    dev1 update

--- Calcuator.java
+++ Calcuator.java
@@ -3,6 +3,31 @@ import java.util.Scanner;
 public class Calculator {
 	public static double performOperation(double num1, double num2, char operator) {
 		
+		double result = 0.0;
+		
+		switch = {
+			case '+':
+				result = num1 + num2;
+				break;
+			case '-':
+				result = num1 - num2;
+				break;
+			case '*':
+				result = num1 * num2;
+				break;
+			case '/':
+				if(num != 0) {
+					result = num1 / num2;
+				}
+				else {
+					System.out.println("0으로 나눌 수 없습니다.");
+				}
+				break;
+			default;
+				System.out.println("올바른 연산자를 입력하세요.");
+				
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
