f89396c8bd1c6109d89c87a60315d90d6429d347
kkang4557 <kkang45597@gmail.com>
2025-07-24 16:13:18 +0900

    dev1 update

--- Calcuator.java
+++ Calcuator.java
@@ -34,14 +34,14 @@ public class CalculatorInput {
 	public static void getInputAndCalculate() {
 		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("간단한 콘솔 계산기 프로그램");
+		System.out.println("간단한 콘솔 계산기 프로그램");
 		//입력 1
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        double num1 = scanner.nextDouble();
+		System.out.print("첫 번째 숫자를 입력하세요: ");
+		double num1 = scanner.nextDouble();
 
 		//입력 2
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        double num2 = scanner.nextDouble();
+		System.out.print("두 번째 숫자를 입력하세요: ");
+		double num2 = scanner.nextDouble();
 
 		System.out.print("사용할 연산자를 입력하세요 (+, -, *, /): ");
 		char operator = scanner.next().charAt(0);
@@ -52,5 +52,5 @@ public class CalculatorInput {
 			System.out.println("결과: " + result);
 		}
 		scanner.close();
-    }
+	}
 }
