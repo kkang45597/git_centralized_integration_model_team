d161627bab5e907c8e89e2a7c1592a568f19c843
yukiriru <yukiriru1222@gmail.com>
2025-07-24 16:11:03 +0900

    2

--- Calcuator.java
+++ Calcuator.java
@@ -39,9 +39,11 @@ public class CalculatorInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("간단한 콘솔 계산기 프로그램");
+		//입력 1
         System.out.print("첫 번째 숫자를 입력하세요: ");
         double num1 = scanner.nextDouble();
 
+		//입력 2
         System.out.print("두 번째 숫자를 입력하세요: ");
         double num2 = scanner.nextDouble();
 
